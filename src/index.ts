d83d5e9fc014af0206e1ea50b478f5f5956651c9
RavinduMihiranga <e18224@eng.pdn.ac.lk>
2024-03-03 18:47:35 +0530

    Adding manage restaurant feature

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
+import { v2 as cloudinary } from "cloudinary";
 import cors from "cors";
 import "dotenv/config";
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
+import restaurantRoute from "./routes/restaurantRoute";
 import userRoute from "./routes/userRoute";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log("Connected to MongoDB");
+});
+
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
@@ -17,6 +26,7 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/user", userRoute);
+app.use("/api/restaurant", restaurantRoute);
 
 app.listen(7000, () => {
   console.log("Server is running on localhost:7000");
